41f30db07dd0f716b44df517b10b7667cb116fd7
HD Koh <hd.koh290293@gmail.com>
2016-04-04 17:01:09 +0800

    Update RoomServiceMgr.java

--- RoomServiceMgr.java
+++ RoomServiceMgr.java
@@ -1,29 +1,43 @@
 package cz2002_assignment;
 
-public class RoomServiceMgr {
+import java.util.ArrayList;
+import java.util.List;
 
+public class RoomServiceMgr {
+    
+    private String roomNumber;
+    private static List<RoomServiceOrder> RoomOrderList;
+    
     RoomServiceMgr() {
-        
+        RoomOrderList = new ArrayList();
     }
     
     public void showMenu() {
-
+        MenuMgr.updateList();
+        MenuMgr.itemList();
+    }
+    
+    public void getItemDescription(int x){
+        MenuMgr.getItemDescription(x);
     }
 
-    public void createOrder() {
-
+    public void createOrder(List<Item> itemOrder, String remarks) {
+        RoomServiceOrder rmOrder = new RoomServiceOrder(itemOrder, remarks);
+        RoomOrderList.add(rmOrder); //Keeps a record of all the orders available
+        rmOrder.printOrder(); //Prints a receipt of the order
     }
 
-    public void updateOrder() {
-
+    public void updateOrder(int orderCode) {
+        // Update order based on the order code
     }
 
     public void removeOrder() {
 
     }
 
-    public void getOrder() {
-
+    public void getOrder(int orderCode) {
+        // Returns the Order based on the order code.
     }
-
+    
+    public void setRoomNo(String roomNo){roomNumber = roomNo;}
 }
