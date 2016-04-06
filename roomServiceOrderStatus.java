14094abb073f7904bd13f56af986af99c3339bb5
HD Koh <hd.koh290293@gmail.com>
2016-04-06 15:53:50 +0800

    Create roomServiceOrderStatus.java

---
+++ roomServiceOrderStatus.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cz2002_assignment;
+
+public enum roomServiceOrderStatus {
+    CONFIRMED, PREPARING, DELIVERED;
+    
+    public String toString(){
+        String format = "";
+        switch (this) {
+            case CONFIRMED:
+                format = "Confirmed";
+                break;
+            case PREPARING:
+                format = "Preparing";
+                break;
+            case DELIVERED:
+                format = "Delivered";
+                break;
+        }
+        return format;
+    }
+}
