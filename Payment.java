7292c252662038ea92a213195530e3b0364c0b8d
HD Koh <hd.koh290293@gmail.com>
2016-04-06 15:52:13 +0800

    Update Payment.java

--- Payment.java
+++ Payment.java
@@ -7,12 +7,20 @@ public class Payment {
     private double roomChargesBill;
 
     Payment() {
-
+        totalBill = 0;
+        roomServiceBill = 0;
+        roomChargesBill = 0;
     }
 
+    public void printBillInvoice(){
+        // print bill invoice (with breakdowns on days of stay, room service order items and its total, tax and total amount)
+    }
     public double getRoomServiceBill() {return roomServiceBill;}
     public double getRoomCharges() {return roomChargesBill;}
-    public double getTotalBill() {return totalBill;}
+    public double getTotalBill() {
+        return totalBill; //For GST in Singapore
+    }
     public void setRoomServiceBill(double billAdd) { roomServiceBill += billAdd;}
+    public void setRoomChargesBill(){}
 
 }
