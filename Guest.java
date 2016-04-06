317f6161ef053096c1fad65d700e2e7080f33889
HD Koh <hd.koh290293@gmail.com>
2016-04-07 00:20:23 +0800

    Update Guest.java

--- Guest.java
+++ Guest.java
@@ -1,35 +1,66 @@
 package cz2002_assignment;
 
+
 public class Guest {
 
     private String name;
-    private String creditCardDet;
-    private String address;
-    private String country;
     private String gender;
     private String identity;
+    private String address;
     private String nationality;
     private int contact;
+    private String creditCardDet;
 
     
-    Guest() {
-        
+    Guest(String n, String g, String i, String a, String N, int c, String ccd) {
+        name = n;
+        gender = g;
+        identity = i;
+        address = a;
+        nationality = N;
+        contact = c;
+        creditCardDet = ccd;
     }
     
     /**
      *
      * @param roomNo
      */
-    public void roomService(int roomNo) {
+    public void roomService(int roomNo) { //Useless
         
     }
 
     public void roomLog() {
-        
+        // Stores the data of room
     }
 
     public void reservationLog() {
         
     }
-
+    public void printDetails() {
+        System.out.println("\n========================");
+        System.out.println("Name: " + name);
+        System.out.println("Gender: " + gender);
+        System.out.println("Address: " + address);
+        System.out.println("Identity: " + identity);
+        System.out.println("Nationality: " + nationality);
+        System.out.println("Contact: " + contact);
+        System.out.println("CreditCardDet: " + creditCardDet);
+        System.out.println("========================\n");
+    }
+    public String getName() {return this.name;}
+    public String getGender() {return this.gender;}
+    public String getAddress() {return this.address;}
+    public String getIdentity() {return this.identity;}
+    public String getNationality() {return this.nationality;}
+    public int getContact() {return this.contact;}
+    public String getCreditCardDet() {return this.creditCardDet;}
+    
+    public void setName(String n){name = n;}
+    public void setGender(String g){gender = g;}
+    public void setAddress(String a){address = a;}
+    public void setIdentity(String i){identity = i;}
+    public void setNationality(String N){nationality = N;}
+    public void setContact(int c){contact = c;}
+    public void setCreditCardDet(String ccd) {creditCardDet = ccd;}
 }
